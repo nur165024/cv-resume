4c5683da896875f72c9788f2ec08cb6e7710df35
nuraalam <nur165024@gmail.com>
2021-01-20 22:26:30 +0600

    cv-resume

---
+++ js/script.js
@@ -0,0 +1,36 @@
+$(function(){
+    // progress Bar part
+    $("#progressBar").skill();
+
+    // filterizr js code
+    $('.filtr-container').filterizr({});
+
+    // venobox
+    $('.venobox').venobox();
+
+    // menu fix
+
+    // var navoffset = $('#manu_bar_item').offset().top;
+
+    // $(window).scroll(function () {
+    //     var scrolling = $(this).scrollTop();
+    //     if (scrolling > navoffset) {
+    //         $("#manu_bar_item").addClass("menu_fix");
+    //     } 
+    //     else {
+    //         $("#manu_bar_item").removeClass("menu_fix");
+    //     }
+    // });
+
+    // manu part
+    // $(function(){
+    //     $('#search_menu').click(function(){
+    //         $('#navbarSupportedContent').hide();
+    //         $('#show_search').show();
+    //     });
+
+        
+    // });
+
+    
+});
